71969e130bbe8bef41a32e88e3fa3f44e0737ef8
Sara Abdullahi Y <sara.abdullahiy@gmail.com>
2025-02-02 00:20:41 +0300

    Add new item form

--- app.py
+++ app.py
@@ -16,6 +16,26 @@ def index():
 def register():
     return render_template("register.html")
 
+@app.route("/new_item")
+def new_item():
+    return render_template("new_item.html")
+
+@app.route("/create_item", methods=["POST"])
+def create_item():
+    title = request.form["title"]
+    author = request.form["author"]
+    year = request.form["year"]
+    genre = request.form["genre"]
+    description = request.form["description"]
+    stars = request.form["stars"]
+    user_id = session["user_id"]
+
+    sql = "INSERT INTO items (title, author, year, genre, description, stars, user_id) VALUES (?, ?, ?, ?, ?, ?, ?)"
+    db.execute(sql, [title, author, year, genre, description, stars, user_id])
+
+    return redirect("/")
+
+
 @app.route("/create", methods=["POST"])
 def create():
     username = request.form["username"]
@@ -37,15 +57,16 @@ def create():
 def login():
     if request.method == "GET":
         return render_template("login.html")
-
     if request.method == "POST":
-
         username = request.form["username"]
         password = request.form["password"]
-        sql = "SELECT password_hash FROM users WHERE username = ?"
-        password_hash = db.query(sql, [username])[0][0]
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result = db.query(sql, [username])[0]
+        user_id = result["id"]
+        password_hash = result["password_hash"]
 
         if check_password_hash(password_hash, password):
+            session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
         else:
@@ -53,5 +74,6 @@ def login():
 
 @app.route("/logout")
 def logout():
+    del session["user_id"]
     del session["username"]
     return redirect("/")
--- schema.sql
+++ schema.sql
@@ -3,3 +3,15 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+
+CREATE TABLE items(
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    author TEXT,
+    year INTEGER,
+    genre TEXT,
+    description TEXT,
+    stars INTEGER,
+    user_id INTEGER REFRENCES users
+);
